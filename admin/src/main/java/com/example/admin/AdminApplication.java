d6b88cee7e2351402db4aab95e1e3b7915ac36c9
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-04-20 01:53:06 +0300

    Created project

---
+++ admin/src/main/java/com/example/admin/AdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.admin;
+
+/**
+ * Hello world!
+ *
+ */
+public class AdminApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
