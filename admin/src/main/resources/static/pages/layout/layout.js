2a1f6d5465a9e684ff498ad2df5d9974892ae3db
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-05-02 19:09:05 +0300

    Admin - Layout: showing admin avatar in navbar

--- admin/src/main/resources/static/pages/layout/layout.js
+++ admin/src/main/resources/static/pages/layout/layout.js
@@ -2,9 +2,14 @@
 const errorMessage = "Виникла помилка!";
 const updateSuccessMessage = "Оновлення успішне!";
 const saveSuccessMessage = "Збереження успішне!";
+const fileValidation = "Зображення не відповідає формату .jpeg, .jpg, .png";
 let token = $("meta[name='_csrf']").attr("content");
 $(document).ready(function () {
     const currHref = window.location.pathname;
+    const currentUrl = window.location.href;
+    const myArray = currentUrl.split("/");
+    let root = myArray[3];
+    $("#nav-avatar").attr("src", "/"+root+"/uploads/"+authenticatedAvatar);
     $('.menu-item a').each(function (i, item) {
         if (currHref.includes($(item).attr('href'))) {
             $(item).parent().addClass('active');
