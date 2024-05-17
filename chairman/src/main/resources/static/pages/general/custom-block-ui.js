59b712f9eb7e4568786cb1cb3a6cd14019eab3b1
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-05-18 01:21:11 +0300

    Chairman - added general scripts for validation, pagination and block ui

---
+++ chairman/src/main/resources/static/pages/general/custom-block-ui.js
@@ -0,0 +1,28 @@
+function blockCardDody() {
+    console.log("block");
+    $('.card-body').block({
+        message: `<div class="col d-flex justify-content-center ">
+                 <!-- Wave -->
+                 <div class="sk-wave sk-primary">
+                   <div class="sk-wave-rect"></div>
+                   <div class="sk-wave-rect"></div>
+                   <div class="sk-wave-rect"></div>
+                   <div class="sk-wave-rect"></div>
+                   <div class="sk-wave-rect"></div>
+                 </div>
+               </div>`,
+        css: {
+            backgroundColor: "transparent",
+            border: "0",
+            position: "center"
+        },
+        overlayCSS: {
+            backgroundColor: "#fff",
+            opacity: 0.8
+        }
+    })
+}
+
+$(document).on('ajaxStop', function () {
+    $('.card-body').unblock();
+})
