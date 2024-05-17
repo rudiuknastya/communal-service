dcd076de5680eb41fbb7be29383a1b791a5a313e
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-05-18 01:13:22 +0300

    Chairman - changed navbar style and added script for layout

--- chairman/src/main/resources/static/pages/layout/layout.js
+++ chairman/src/main/resources/static/pages/layout/layout.js
@@ -1,5 +1,18 @@
 
 let token = $("meta[name='_csrf']").attr("content");
+$(document).ready(function () {
+    const currHref = window.location.pathname;
+    const currentUrl = window.location.href;
+    const myArray = currentUrl.split("/");
+    let root = myArray[3];
+    $("#nav-avatar").attr("src", "/"+root+"/uploads/"+authenticatedAvatar);
+    $('.menu-item a').each(function (i, item) {
+        if (currHref.includes($(item).attr('href'))) {
+            $(item).parent().addClass('active');
+            $(item).css("color","#7367f0");
+        }
+    });
+});
 
 $("#logoutLink").on("click", function (e) {
     e.preventDefault();
