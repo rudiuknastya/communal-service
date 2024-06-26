89e89feec05617b535c9838170a2582a55082870
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-06-26 13:33:16 +0300

    User - Chat - done chat page

---
+++ user/src/main/java/com/example/user/controller/ChatController.java
@@ -0,0 +1,15 @@
+package com.example.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+    @GetMapping("")
+    public ModelAndView getContactsPagePage(){
+        return new ModelAndView("chat/chat");
+    }
+}
