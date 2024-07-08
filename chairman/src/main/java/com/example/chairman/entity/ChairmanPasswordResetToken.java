c04002e41e025bc4b2d8cc5f2c00d64d63367f66
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2024-07-08 19:08:21 +0300

    Chairman - changed expirationDate from Instant to LocalDateTime in ChairmanPasswordResetToken

--- chairman/src/main/java/com/example/chairman/entity/ChairmanPasswordResetToken.java
+++ chairman/src/main/java/com/example/chairman/entity/ChairmanPasswordResetToken.java
@@ -3,6 +3,7 @@ package com.example.chairman.entity;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 @Entity
@@ -14,9 +15,9 @@ public class ChairmanPasswordResetToken {
     @Column(unique = true, nullable = false)
     private String token;
     @Column(nullable = false)
-    private Instant expirationDate;
+    private LocalDateTime expirationDate;
     private boolean used;
-    private static final int EXPIRATION = 20;
+    private final int EXPIRATION = 20;
     @OneToOne
     @JoinColumn(nullable = false, name = "chairman_id", referencedColumnName = "id")
     private Chairman chairman;
@@ -49,11 +50,11 @@ public class ChairmanPasswordResetToken {
         this.expirationDate = calculateExpirationDate();
     }
 
-    public Instant getExpirationDate() {
+    public LocalDateTime getExpirationDate() {
         return expirationDate;
     }
-    private Instant calculateExpirationDate(){
-        return Instant.now().plus(EXPIRATION, ChronoUnit.MINUTES);
+    private LocalDateTime calculateExpirationDate(){
+        return LocalDateTime.now().plusMinutes(EXPIRATION);
     }
 
     public boolean isUsed() {
